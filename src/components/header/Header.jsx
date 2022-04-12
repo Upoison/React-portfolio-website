24c357443c4f91a3a746e5de58be164c6de85ca3
upoison <destivengarzon@gmail.com>
2022-04-11 19:36:34 -0500

    Adding changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     <header>
       <div className="container header__container">
         <h5>Hello I'm</h5>
-        <h1>David Garzón</h1>
+        <h1>Sofia Vargas</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
         <HeaderSocials />
